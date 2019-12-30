6a5e258be585131f7f065a9803f500348a32ab33
ajfisher <ajfisher.td@gmail.com>
2019-12-30 15:50:46 +1100

    tests: Added tests for .set_firmware_details() scenarios

--- test/firmware_configuration.test.js
+++ test/firmware_configuration.test.js
@@ -11,13 +11,9 @@ const interchange_client = require('../lib/interchange_client');
 
 const interchange_get_info = () => describe('1. Firmware reading works correctly', () => {
   beforeAll(() => {
-    // make a temporary serialport
-    // MockBinding.createPort('/dev/dummy', {echo: true, record: true});
-    // SerialPort.Binding = MockBinding;
   });
 
   afterAll(() => {
-    // MockBinding.close();
   });
 
   beforeEach(() => {
@@ -153,4 +149,139 @@ const interchange_get_info = () => describe('1. Firmware reading works correctly
   });
 });
 
+
+const interchange_set_details = () => describe('2. Setting the firmware details', () => {
+  let mock_hook; // use this to be able to access the mock implementation later.
+
+  afterEach(() => {
+    mock_hook = undefined; // clean up any references
+  });
+
+  beforeEach(() => {
+    jest.resetModules();
+
+    // mock up the interchange client key events.
+    interchange_client.Client.mockImplementation(() => {
+      // need to extend this off the EventEmitter class.
+      const self = new Events.EventEmitter();
+
+      let port = null; // use port to determine the behaviour of get info
+
+      self.set_details = jest.fn().mockImplementation((settings, cb) => {
+        if (port === '/dev/dummy_sd_pass') {
+          cb();
+        }
+      });
+
+      mock_hook = self.set_details;
+
+      self.close = () => {
+        return;
+      };
+
+      Object.defineProperties(self, {
+        'port': {
+          set(p) {
+            // use this to throw an error on the port set up
+            if (p == '/dev/dummy_port_open_error') {
+              setImmediate(() => {
+                self.emit('error', new Error('Cannot open /dev/dummy_port_open_error'));
+              });
+            } else {
+              setImmediate(() => {
+                // set the port for the get_info behaviours.
+                port = p;
+                self.emit('ready');
+              });
+            }
+          }
+        }
+      });
+      return self;
+    });
+
+    interchange = new Interchange();
+  });
+
+  test('2.1 If no port is set then .set_details should reject', (done) => {
+    expect.assertions(1);
+    return interchange.set_firmware_details()
+      .catch(err => {
+        expect(err.toString()).toMatch(/No port specified/);
+        done();
+      });
+  });
+
+  test('2.2 Error during port opening .set_firmware_details() should emit error and reject', (done) => {
+    expect.assertions(1);
+    // this needs options
+    const opts = {address: 0x27, firmwareID: 0x01, creatorID: 0x02};
+    return interchange.set_firmware_details('/dev/dummy_port_open_error', opts)
+      .catch(err => {
+        expect(err.toString()).toMatch(/Cannot open \/dev\/dummy_port_open_error/);
+        done();
+      });
+  });
+
+  test('2.3 .set_firmware_details should reject if supplied opts are missing details', (done) => {
+    expect.assertions(3);
+    // first check no address supplied
+    const port = '/dev/dummy_options_error';
+    return interchange.set_firmware_details(port, {firmwareID: 0x01, creatorID: 0x02})
+      .catch(err => {
+        expect(err.toString()).toMatch(/No default address supplied/);
+
+        // now check no firmware ID
+        return interchange.set_firmware_details(port, {address: 0x27, creatorID: 0x02})
+          .catch(err2 => {
+            expect(err2.toString()).toMatch(/No firmware ID supplied/);
+
+            // now last check no creator ID
+            return interchange.set_firmware_details(port, {address: 0x27, firmwareID: 0x01})
+              .catch(err3 => {
+                expect(err3.toString()).toMatch(/No creator ID supplied/);
+                done();
+              });
+          });
+      });
+  });
+
+  test('2.4 .set_firmware_details() should pass a correct settings object to ic.set_info', (done) => {
+    expect.assertions(9);
+    const port = '/dev/dummy_sd_pass';
+    const opts = {address: '0x27', firmwareID: '0x01', creatorID: '0x02'};
+    return interchange.set_firmware_details(port, opts)
+      .then(() => {
+        const args = mock_hook.mock.calls[0];
+        expect(args[0]).toBeDefined();
+        expect(args[0].firmwareID).toBeDefined();
+        expect(args[0].creatorID).toBeDefined();
+        expect(args[0].i2c_address).toBeDefined();
+        expect(args[0].use_custom_address).toBeDefined();
+        expect(args[0].firmwareID).toBe(0x01);
+        expect(args[0].creatorID).toBe(0x02);
+        expect(args[0].i2c_address).toBe(0x27);
+        expect(args[0].use_custom_address).toBe(0);
+        done();
+      });
+  });
+
+  test('2.5 .set_firmware_details() should pass a correct settings object when custom i2c address set', (done) => {
+    expect.assertions(5);
+    const port = '/dev/dummy_sd_pass';
+    const opts = {address: '0x27', i2c_address: '0x42', firmwareID: '0x01', creatorID: '0x02'};
+    return interchange.set_firmware_details(port, opts)
+      .then(() => {
+        const args = mock_hook.mock.calls[0];
+        expect(args[0]).toBeDefined();
+        expect(args[0].i2c_address).toBeDefined();
+        expect(args[0].use_custom_address).toBeDefined();
+        expect(args[0].i2c_address).toBe(0x42);
+        expect(args[0].use_custom_address).toBe(1);
+        done();
+      });
+  });
+});
+
 interchange_get_info();
+interchange_set_details();
